47823d1d4538dbf444d571330fe5c00fa3a96739
jmeng <spirit.jay@163.com>
2020-03-22 02:50:45 +0800

    test: issue#948 committing tuple-like object

---
+++ javers-core/src/test/groovy/org/javers/core/cases/Case948CommitTupleLikeObject.groovy
@@ -0,0 +1,45 @@
+package org.javers.core.cases
+
+import org.javers.core.Javers
+import org.javers.core.JaversBuilder
+import org.javers.core.commit.Commit
+
+import spock.lang.Specification
+
+class Case948CommitTupleLikeObject extends Specification {
+	class Pair<L, R> {
+		L left;
+		R right;
+
+		public Pair(L left, R right) {
+			this.left = left;
+			this.right = right;
+		}
+
+		@Override
+		public String toString() {
+			return "Pair [left=" + left + ", right=" + right + "]";
+		}
+	}
+
+	def "should tracking work when committing tuple-like object"(){
+		given:
+		Javers javers = JaversBuilder.javers().build();
+		//Pair<Long, String> obj = org.apache.commons.lang3.tuple.MutablePair.of(1L, "foo");
+		Pair<Long, String> obj = new Pair<>(1L, "foo");
+		//    Map<Long,String> obj = new HashMap();
+		//    obj.put(1L+"", "foo")
+
+		when:
+		javers.commit("jay", obj);
+
+		//obj.setValue("bar");
+		obj.right = "bar";
+		//    obj.put(1L, "bar")
+
+		Commit commit = javers.commit("jay", obj);
+
+		then:
+		commit.getChanges().size() == 1
+	}
+}
